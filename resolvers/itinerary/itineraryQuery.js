c33918d291355429fb127f862527402551e89e05
geomin76 <geom@vt.edu>
2021-04-11 23:18:59 -0400

    Fixing mongo query

--- resolvers/itinerary/itineraryQuery.js
+++ resolvers/itinerary/itineraryQuery.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb')
 const ObjectID = mongodb.ObjectId;
 
 const findItinerary = async (parent, args, context, info) => {
-    const result = await context.database.collection('blogs').findOne({_id: new ObjectID(args.id)})
+    const result = await context.database.collection('itineraries').findOne({_id: new ObjectID(args.id)})
 
     return result
 }
