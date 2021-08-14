576253a2b1444d9b3864f7c0607092ae2c8248a9
Geo Min <31418725+geomin76@users.noreply.github.com>
2021-08-14 12:56:27 -0400

    Notes (#11)
    * fixing query
    
    * Adding notes mutations

--- resolvers/trip/tripMutation.js
+++ resolvers/trip/tripMutation.js
@@ -7,6 +7,8 @@ const ObjectID = mongodb.ObjectId;
  */
 const createTrip = async (parent, args, context, info) => {
     const data = {}
+    data['notes'] = []
+    data['trip'] = {}
 
     // trip is created through "/create/trip"
     if (args.beginning && args.end) {
@@ -21,9 +23,9 @@ const createTrip = async (parent, args, context, info) => {
             var month = ('0' + (d.getMonth() + 1)).slice(-2);
             var date = ('0' + (d.getDate())).slice(-2);
             var insertedDate = `${year}-${month}-${date}`;
-            data[insertedDate] = []
+            data['trip'][insertedDate] = []
         }
-
+        
         const result = await context.database.collection('trips').insertOne(data)
             .catch(err => console.error(`Create failed with error: ${err}`))
         return result.ops[0]
@@ -44,7 +46,7 @@ const createTrip = async (parent, args, context, info) => {
 const insertElementToTrip = async (parent, args, context, info) => {
     const filter = { _id: new ObjectID(args.id) }  
     var data = {}
-    data[`${args.date}`] = args.element
+    data[`trip.${args.date}`] = args.element
 
     const result = await context.database.collection('trips').updateOne(filter, { $push: data })
         .catch(err => console.error(`Update failed with error: ${err}`))
@@ -74,29 +76,34 @@ const updateTripDate = async (parent, args, context, info) => {
         var month = ('0' + (d.getMonth() + 1)).slice(-2);
         var date = ('0' + (d.getDate())).slice(-2);
         var insertedDate = `${year}-${month}-${date}`;
-        if (document[insertedDate] === undefined) {
-            document[insertedDate] = []
+        if (document['trip'][insertedDate] === undefined) {
+            document['trip'][insertedDate] = []
         }
         outOfRangeDateChecker.push(insertedDate);
     }
 
     // removing old dates that don't overlap
-    for (var i in document) {
-        if (i !== "_id" && !outOfRangeDateChecker.includes(i)) {
-            delete document[i]
+    for (var i in document['trip']) {
+        if (!outOfRangeDateChecker.includes(i)) {
+            delete document['trip'][i]
         }
     }
 
     // sorting
-    const ordered = Object.keys(document).sort().reduce(
+    const ordered = Object.keys(document['trip']).sort().reduce(
         (obj, key) => { 
-          obj[key] = document[key]; 
+          obj[key] = document['trip'][key]; 
           return obj;
         }, 
         {}
-      );
+    );
 
-    const result = await context.database.collection('trips').replaceOne(filter, ordered)
+    const newDocument = {
+        "trip": ordered,
+        "notes": document['notes']
+    }
+
+    const result = await context.database.collection('trips').replaceOne(filter, newDocument)
         .catch(err => console.error(`Update failed with error: ${err}`));
 
     return result.ops[0];
@@ -111,11 +118,11 @@ const updateTripDate = async (parent, args, context, info) => {
 const swapElementsInTrip = async (parent, args, context, info) => {
     const document = await context.database.collection('trips').findOne({_id: new ObjectID(args.id)})
     const filter = { _id: new ObjectID(args.id) }
-    const newElem = document[`${args.date}`]
+    const newElem = document['trip'][`${args.date}`]
     const [removed] = newElem.splice(args.firstIndex, 1);
     newElem.splice(args.secondIndex, 0, removed);
     let data = {}
-    data[`${args.date}`] = newElem
+    data[`trip.${args.date}`] = newElem
 
     const result = await context.database.collection('trips').updateOne(filter, { $set: data })
         .catch(err => console.error(`Update failed with error: ${err}`))
@@ -130,9 +137,9 @@ const swapElementsInTrip = async (parent, args, context, info) => {
  const deleteElementFromTrip = async (parent, args, context, info) => {
     const filter = { _id: new ObjectID(args.id) }    
     var dataForUnset = {}
-    dataForUnset[`${args.date}.${args.index}`] = 1
+    dataForUnset[`trip.${args.date}.${args.index}`] = 1
     var dataForPull = {}
-    dataForPull[`${args.date}`] = null
+    dataForPull[`trip.${args.date}`] = null
 
     await context.database.collection('trips').updateOne(filter, { $unset: dataForUnset })
         .catch(err => console.error(`Update failed with error: ${err}`))
@@ -143,6 +150,61 @@ const swapElementsInTrip = async (parent, args, context, info) => {
     return result.modifiedCount;
 }
 
+/**
+ * @argument id The unique identifier for the mongodb document
+ * @argument date The date array the element wants to push to
+ * @argument element The JSON sub-element for Trip content (experience, txt, image, etc.)
+ */
+const insertElementToNotes = async (parent, args, context, info) => {
+    const filter = { _id: new ObjectID(args.id) }  
+    var data = {}
+    data[`notes`] = args.element
+
+    const result = await context.database.collection('trips').updateOne(filter, { $push: data })
+        .catch(err => console.error(`Update failed with error: ${err}`))
+    return result.modifiedCount;
+}
+
+/**
+ * @argument id The unique identifier for the mongodb document
+ * @argument date The date array the element wants to push to
+ * @argument firstIndex The first index to remove the element from
+ * @argument secondIndex The second index to insert the removed element into
+ */
+const swapElementsInNotes = async (parent, args, context, info) => {
+    const document = await context.database.collection('trips').findOne({_id: new ObjectID(args.id)})
+    const filter = { _id: new ObjectID(args.id) }
+    const newElem = document['notes']
+    const [removed] = newElem.splice(args.firstIndex, 1);
+    newElem.splice(args.secondIndex, 0, removed);
+    let data = {}
+    data[`notes`] = newElem
+
+    const result = await context.database.collection('trips').updateOne(filter, { $set: data })
+        .catch(err => console.error(`Update failed with error: ${err}`))
+    return result.modifiedCount;
+}
+
+/**
+ * @argument id The unique identifier for the mongodb document
+ * @argument date The date array the element wants to delete from
+ * @argument index The index of the JSON sub-element
+ */
+const deleteElementFromNotes = async (parent, args, context, info) => {
+    const filter = { _id: new ObjectID(args.id) }    
+    var dataForUnset = {}
+    dataForUnset[`notes.${args.index}`] = 1
+    var dataForPull = {}
+    dataForPull[`notes`] = null
+
+    await context.database.collection('trips').updateOne(filter, { $unset: dataForUnset })
+        .catch(err => console.error(`Update failed with error: ${err}`))
+
+    const result = await context.database.collection('trips').updateOne(filter, { $pull: dataForPull })
+        .catch(err => console.error(`Update failed with error: ${err}`))
+
+    return result.modifiedCount;
+}
 
 const deleteTrip = async (parent, args, context, info) => {
     const result = await context.database.collection('trips').deleteOne({_id: new ObjectID(args.id)})
@@ -157,5 +219,8 @@ module.exports = {
     updateTripDate,
     swapElementsInTrip,
     deleteElementFromTrip,
+    insertElementToNotes,
+    swapElementsInNotes,
+    deleteElementFromNotes,
     deleteTrip
 }
